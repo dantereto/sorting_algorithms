48cca5407857bca9a64410327abd978641d39b61
dantereto <2247@holbertonschool.com>
2021-06-14 16:36:21 +0000

    buble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 #include "sort.h"
-
+/**
+ * swap - swap the numbers od the array
+ *
+ * @yp: firts element
+ * @xp: second element
+ */
 void swap(int *xp, int *yp)
 {
-  int temp = *xp;
-  *xp = *yp;
-  *yp = temp;
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
 }
 /**
- * print_list - Prints a list of integers
+ * bubble_sort - a bad sort
  *
- * @list: The list to be printed
+ * @array: the size of the array
+ * @size: the size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-  int flag;
-  size_t i, j;
-  for (i = 0; i < size; i++)
-    {
-      flag = 0;
-      for (j = 0; j < size - i - 1; j++)
-	{
-	  if (array[j] > array[j + 1])
-	    {
-	      swap(&array[j], &array[j+1]);
-	      print_array(array, size);
-	      flag = 1;
-	    }
-	}
-      if (flag == 0)
-	break;
-    }
+int flag;
+size_t i, j;
+for (i = 0; i < size; i++)
+{
+flag = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+flag = 1;
+}
+}
+if (flag == 0)
+break;
+}
 }
