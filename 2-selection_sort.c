88435e35eebe818f5d75bb7a4e41088048866909
dantereto <2247@holbertonschool.com>
2021-06-14 16:39:03 +0000

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,28 +2,29 @@
 #include "sort.h"
 
 /**
- * print_list - Prints a list of integers
+ * selection_sort - selection sort
  *
- * @list: The list to be printed
+ * @array: The array
+ * @size: the size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-  int temp;
-  size_t i, j;
-  if (array == NULL || size < 2)
-    return;
-  for (i = 0; i < size-1; i++)
-    {
-      size_t min = i;
-      for (j = i+1; j < size; j++)
-	if (array[j] < array[min])
-	  min = j;
-      if (min != i)
-	{
-	  temp = array[min];
-	  array[min] = array[i];
-	  array[i] = temp;
-	  print_array(array, size);
-	}
-    }
+int temp;
+size_t i, j;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+size_t min = i;
+for (j = i + 1; j < size; j++)
+if (array[j] < array[min])
+min = j;
+if (min != i)
+{
+temp = array[min];
+array[min] = array[i];
+array[i] = temp;
+print_array(array, size);
+}
+}
 }
