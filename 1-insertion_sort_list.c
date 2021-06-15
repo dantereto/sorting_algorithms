5398aa80cca1f1b6119de7a86cbce2431e7c75df
dantereto <2247@holbertonschool.com>
2021-06-15 20:42:49 +0000

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * insertion_sort_list - insertion sort with a list
+ *
+ * @list: the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *i = *list;
+if (list == '\0' || (*list)->next == '\0')
+return;
+while (i)
+{
+while (i->prev != '\0' && i->prev->n > i->n)
+{
+i->prev->next = i->next;
+if (i->next != '\0')
+i->next->prev = i->prev;
+i->next = i->prev;
+i->prev = i->next->prev;
+i->next->prev = i;
+if (i->prev != '\0')
+i->prev->next = i;
+else
+*list = i;
+print_list(*list);
+}
+i = i->next;
+}
+}
